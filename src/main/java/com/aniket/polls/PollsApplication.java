7729c0f9d57055adfd280588384e2dd54f73b61e
Aniket Rai <aniket.rai@gupshup.io>
2024-09-16 23:58:32 +0530

    init spring

---
+++ src/main/java/com/aniket/polls/PollsApplication.java
@@ -0,0 +1,13 @@
+package com.aniket.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+
+}
