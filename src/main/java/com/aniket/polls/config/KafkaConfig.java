9a8b1aa0385f48e77f98496495bcc8490e4b8da9
Aniket Rai <aniket.rai@gupshup.io>
2024-09-17 00:27:57 +0530

    kafak and redis integrated

--- src/main/java/com/aniket/polls/config/KafkaConfig.java
+++ src/main/java/com/aniket/polls/config/KafkaConfig.java
@@ -1,3 +1,5 @@
+package com.aniket.polls.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
